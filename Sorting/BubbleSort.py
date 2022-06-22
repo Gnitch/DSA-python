116b76fd6275de7c58767b9d7b192d449dfb18ff
Sujoy D'cunha <48491386+Gnitch@users.noreply.github.com>
2022-06-22 13:26:18 +0530

    Update BubbleSort.py

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -9,7 +9,7 @@ print('Original array:',arr)
 for i in range(len-1) :
     for j in range(len-i-1) :
         if arr[j] > arr[j+1] :
-            array[j], array[j+1] = array[j+1], array[j] #Swap
+            arr[j], arr[j+1] = arr[j+1], arr[j] #Swap
    
 
 print('Sorted Array:',arr)
